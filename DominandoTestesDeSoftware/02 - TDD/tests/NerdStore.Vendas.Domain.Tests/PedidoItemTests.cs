0afd071e10848eba19b27ee0004e37fecb752469
wenderson1 <wenderson.farias10@gmail.com>
2024-08-15 15:30:51 -0700

    Initial commit

---
+++ DominandoTestesDeSoftware/02 - TDD/tests/NerdStore.Vendas.Domain.Tests/PedidoItemTests.cs
@@ -0,0 +1,15 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public class PedidoItemTests
+    {
+        [Fact(DisplayName = "Novo Item Pedido com unidades abaixo do permitido")]
+        [Trait("Categoria", "Vendas - Pedido Item")]
+        public void AdicionarItemPedido_UnidadesItemAbaixoDoPermitido_DeveRetornarException()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<DomainException>(() => new PedidoItem(Guid.NewGuid(), "Produto Teste", Pedido.MIN_UNIDADES_ITEM - 1, 100));
+        }
+    }
+}
---
+++ DominandoTestesDeSoftware/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/PedidoApiTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Net.Http.Json;
+using Features.Tests;
+using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.MVC.Models;
+using NerdStore.WebApp.Tests.Config;
+
+namespace NerdStore.WebApp.Tests
+{
+    [TestCaseOrderer("Features.Tests.PriorityOrderer", "Features.Tests")]
+    [Collection(nameof(IntegrationApiTestsFixtureCollection))]
+    public class PedidoApiTests
+    {
+        private readonly IntegrationTestsFixture<Program> _testsFixture;
+
+        public PedidoApiTests(IntegrationTestsFixture<Program> testsFixture)
+        {
+            _testsFixture = testsFixture;
+        }
+
+        [Fact(DisplayName = "Adicionar item em novo pedido"), TestPriority(1)]
+        [Trait("Categoria", "Integração API - Pedido")]
+        public async Task AdicionarItem_NovoPedido_DeveRetornarComSucesso()
+        {
+            // Arrange
+            var itemInfo = new ItemViewModel
+            {
+                Id = new Guid("6ecaaa6b-ad9f-422c-b3bb-6013ec27c4bb"),
+                Quantidade = 2
+            };
+
+            await _testsFixture.RealizarLoginApi();
+            _testsFixture.Client.AtribuirToken(_testsFixture.UsuarioToken);
+
+            // Act
+            var postResponse = await _testsFixture.Client.PostAsJsonAsync("api/carrinho", itemInfo);
+
+            // Assert
+            postResponse.EnsureSuccessStatusCode();
+        }
+
+        [Fact(DisplayName = "Remover item em pedido existente"), TestPriority(2)]
+        [Trait("Categoria", "Integração API - Pedido")]
+        public async Task RemoverItem_PedidoExistente_DeveRetornarComSucesso()
+        {
+            // Arrange
+            var produtoId = new Guid("191ddd3e-acd4-4c3b-ae74-8e473993c5da");
+            await _testsFixture.RealizarLoginApi();
+            _testsFixture.Client.AtribuirToken(_testsFixture.UsuarioToken);
+
+            // Act
+            var deleteResponse = await _testsFixture.Client.DeleteAsync($"api/carrinho/{produtoId}");
+
+            // Assert
+            deleteResponse.EnsureSuccessStatusCode();
+        }
+    }
+}
